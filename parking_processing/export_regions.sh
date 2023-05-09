1d73342f3ecedae8274f2c2db335e32705e26c19
gislars <lars@lingner.eu>
2023-05-09 21:54:07 +0200

    Add region Bietigheim-Bissingen to export-script

--- parking_processing/export_regions.sh
+++ parking_processing/export_regions.sh
@@ -11,7 +11,10 @@ do
     mkdir -p /config/export/${REGION}
     psql -t -c "SELECT public.export_parking_geojson('${REGION}'::text);" -o /config/export/region_${REGION}.geojson
     cp /config/export/region_${REGION}.geojson /config/export/${REGION}/
-    ogr2ogr -f GPKG /config/export/${REGION}/parking_segments_${REGION}.gpkg PG:"service='${PG_SERVICE}'" -nln "parking_segments" -sql "SELECT p.* FROM processing.parking_segments p, meta.spatialfilter s WHERE s.name = '${REGION}'::text AND ST_Intersects(p.geog, s.geom::geography)"
-    ogr2ogr -f GPKG /config/export/${REGION}/parking_spaces_${REGION}.gpkg PG:"service='${PG_SERVICE}'" -nln "parking_spaces" -sql "SELECT p.* FROM processing.parking_spaces p, meta.spatialfilter s WHERE s.name = '${REGION}'::text AND ST_Intersects((p.geom)::geography, s.geom::geography)"
-    ogr2ogr -f GPKG /config/export/${REGION}/parking_lanes_${REGION}.gpkg PG:"service='${PG_SERVICE}'" -nln "parking_lanes" -sql "SELECT p.* FROM processing.parking_lanes p, meta.spatialfilter s WHERE s.name = '${REGION}'::text AND ST_Intersects((p.geom)::geography, s.geom::geography)"
+    python3 /config/export_data.py --output_file_name /config/export/${REGION}/parking_segments_${REGION}.gpkg --table_name processing.parking_segments --region_name ${REGION}
+    python3 /config/export_data.py --output_file_name /config/export/${REGION}/parking_spaces_${REGION}.gpkg --table_name processing.parking_spaces --region_name ${REGION}
+    python3 /config/export_data.py --output_file_name /config/export/${REGION}/parking_lanes_${REGION}.gpkg --table_name processing.parking_lanes --region_name ${REGION}
+    #ogr2ogr -f GPKG /config/export/${REGION}/parking_segments_${REGION}.gpkg PG:"service='${PG_SERVICE}'" -gt 65536 --config OGR_SQLITE_CACHE=512 -nln "parking_segments" -sql "SELECT p.* FROM processing.parking_segments p, meta.spatialfilter s WHERE s.name = '${REGION}'::text AND ST_Intersects(p.geog, s.geom::geography)"
+    #ogr2ogr -f GPKG /config/export/${REGION}/parking_spaces_${REGION}.gpkg PG:"service='${PG_SERVICE}'" -gt 65536 --config OGR_SQLITE_CACHE=512 -nln "parking_spaces" -sql "SELECT p.* FROM processing.parking_spaces p, meta.spatialfilter s WHERE s.name = '${REGION}'::text AND ST_Intersects((p.geom)::geography, s.geom::geography)"
+    #ogr2ogr -f GPKG /config/export/${REGION}/parking_lanes_${REGION}.gpkg PG:"service='${PG_SERVICE}'" -gt 65536 --config OGR_SQLITE_CACHE=512 -nln "parking_lanes" -sql "SELECT p.* FROM processing.parking_lanes p, meta.spatialfilter s WHERE s.name = '${REGION}'::text AND ST_Intersects((p.geom)::geography, s.geom::geography)"
 done
