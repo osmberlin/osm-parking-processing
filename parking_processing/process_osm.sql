e6c1a8004a839fcea59a327cde3bcd5e8c6fc970
SupaplexOSM <supaplex@riseup.net>
2023-07-22 11:49:20 +0200

    added on kerb stats

--- parking_processing/process_osm.sql
+++ parking_processing/process_osm.sql
@@ -1672,6 +1672,9 @@ SELECT
   COALESCE(ROUND((SUM(ST_Length(h.geog)) FILTER (WHERE dual_carriageway IS NULL AND (parking_left_position IN ('lane') OR parking_right_position IN ('lane'))))::numeric / 1000, 1), 0) +
   COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IN ('lane') OR parking_right_position IN ('lane'))))::numeric / 1000, 1), 0) AS lane_km,
 
+  COALESCE(ROUND((SUM(ST_Length(h.geog)) FILTER (WHERE dual_carriageway IS NULL AND (parking_left_position IN ('on_kerb') OR parking_right_position IN ('on_kerb'))))::numeric / 1000, 1), 0) +
+  COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IN ('on_kerb') OR parking_right_position IN ('on_kerb'))))::numeric / 1000, 1), 0) AS on_kerb_km,
+
   COALESCE(ROUND((SUM(ST_Length(h.geog)) FILTER (WHERE dual_carriageway IS NULL AND (parking_left_position IS NULL OR parking_right_position IS NULL)))::numeric / 1000, 1), 0) +
   COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IS NULL OR parking_right_position IS NULL)))::numeric / 1000, 1), 0) AS d_other_km,
 
@@ -1679,6 +1682,8 @@ SELECT
   COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IN ('street_side') OR parking_right_position IN ('street_side'))))::numeric / 1000, 1), 0) +
   COALESCE(ROUND((SUM(ST_Length(h.geog)) FILTER (WHERE dual_carriageway IS NULL AND (parking_left_position IN ('lane') OR parking_right_position IN ('lane'))))::numeric / 1000, 1), 0) +
   COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IN ('lane') OR parking_right_position IN ('lane'))))::numeric / 1000, 1), 0) +
+  COALESCE(ROUND((SUM(ST_Length(h.geog)) FILTER (WHERE dual_carriageway IS NULL AND (parking_left_position IN ('on_kerb') OR parking_right_position IN ('on_kerb'))))::numeric / 1000, 1), 0) +
+  COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IN ('on_kerb') OR parking_right_position IN ('on_kerb'))))::numeric / 1000, 1), 0) +
   COALESCE(ROUND((SUM(ST_Length(h.geog)) FILTER (WHERE dual_carriageway IS NULL AND (parking_left_position IS NULL OR parking_right_position IS NULL)))::numeric / 1000, 1), 0) +
   COALESCE(ROUND((SUM(ST_Length(h.geog) / 2) FILTER (WHERE dual_carriageway = true AND (parking_left_position IS NULL OR parking_right_position IS NULL)))::numeric / 1000, 1), 0) AS sum_km,
   ROUND((SUM(ST_Length(h.geog)) / 1000)::numeric, 1) "length_wo_dual_carriageway",
