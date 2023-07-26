2bac1e121024558234395bba3d5ebb04c42b4c6e
gislars <lars@lingner.eu>
2023-07-26 09:18:12 +0200

    Update import script, remove custom path for osm2pgsql, do osm2pgsql status only in main loop, add label attribute for spatialfilter

--- parking_import/import_osm.sh
+++ parking_import/import_osm.sh
@@ -24,147 +24,147 @@ fi
 
 export PGSERVICE=${PGSERVICE}
 
-O2P_DIRECTORY=/usr/local/bin/
-
 function importosm () {
 
-    PBF_MERGED_FILENAME=merged.osm.pbf
-    PBF_UPTODATE_FILENAME=uptodate.osm.pbf
+  PBF_MERGED_FILENAME=merged.osm.pbf
+  PBF_UPTODATE_FILENAME=uptodate.osm.pbf
 
-    if [ -f "/data/${PBF_MERGED_FILENAME}" ] ; then
-        rm -f "/data/${PBF_MERGED_FILENAME}"
-    fi
+  if [ -f "/data/${PBF_MERGED_FILENAME}" ] ; then
+    rm -f "/data/${PBF_MERGED_FILENAME}"
+  fi
 
-    if [ -f "/data/${PBF_UPTODATE_FILENAME}" ] ; then
-        rm -f "/data/${PBF_UPTODATE_FILENAME}"
-    fi
+  if [ -f "/data/${PBF_UPTODATE_FILENAME}" ] ; then
+    rm -f "/data/${PBF_UPTODATE_FILENAME}"
+  fi
 
-    #TODO check file size and warn about it
+  #TODO check file size and warn about it
 
-    echo "merging all present osm pbf files"
-    echo osmium merge -o /data/${PBF_MERGED_FILENAME} "/osm"/*.pbf
-    #TODO output more information like which files, timestamp
-    osmium merge -o /data/${PBF_MERGED_FILENAME} "/osm"/*.pbf
+  echo "merging all present osm pbf files"
+  echo osmium merge -o /data/${PBF_MERGED_FILENAME} "/osm"/*.pbf
+  #TODO output more information like which files, timestamp
+  osmium merge -o /data/${PBF_MERGED_FILENAME} "/osm"/*.pbf
 
-    #TODO output current timestamp, updated timestamp
-    if [ ! -f "/data/${PBF_UPTODATE_FILENAME}" ] ; then
-        osmium fileinfo "/data/${PBF_MERGED_FILENAME}"
-        pyosmium-up-to-date \
-            --outfile "/data/${PBF_UPTODATE_FILENAME}" \
-            --ignore-osmosis-headers \
-            --server https://planet.osm.org/replication/minute/ \
-            /data/${PBF_MERGED_FILENAME}
-        osmium fileinfo "/data/${PBF_UPTODATE_FILENAME}"
-        echo "finished updating merged osm data"
-    fi
+  #TODO output current timestamp, updated timestamp
+  if [ ! -f "/data/${PBF_UPTODATE_FILENAME}" ] ; then
+    osmium fileinfo "/data/${PBF_MERGED_FILENAME}"
+    pyosmium-up-to-date \
+      --outfile "/data/${PBF_UPTODATE_FILENAME}" \
+      --ignore-osmosis-headers \
+      --server https://planet.osm.org/replication/minute/ \
+      /data/${PBF_MERGED_FILENAME}
+    osmium fileinfo "/data/${PBF_UPTODATE_FILENAME}"
+    echo "finished updating merged osm data"
+  fi
 
-    PG_SCHEMA_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM information_schema.schemata WHERE schema_name = '${PG_SCHEMA_IMPORT}');")
+  PG_SCHEMA_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM information_schema.schemata WHERE schema_name = '${PG_SCHEMA_IMPORT}');")
 
-    if [ ! "${PG_SCHEMA_EXISTS}" = "t" ]
-    then
-        echo "create schema PG_SCHEMA_IMPORT"
-        psql \
-          -c "CREATE SCHEMA IF NOT EXISTS ${PG_SCHEMA_IMPORT} AUTHORIZATION ${PG_ENV_POSTGRES_USER};"
-    fi
-
-    echo "run osm import"
-
+  if [ ! "${PG_SCHEMA_EXISTS}" = "t" ]
+  then
+    echo "create schema PG_SCHEMA_IMPORT"
     psql \
-      -c "CREATE SCHEMA IF NOT EXISTS ${PG_SCHEMA_MIDDLE} AUTHORIZATION ${PG_ENV_POSTGRES_USER};"
-    ${O2P_DIRECTORY}osm2pgsql \
-        --create \
-        --slim \
-        --cache 2000 \
-        --output=flex \
-        --style=/config/parkraum.lua \
-        --middle-schema="${PG_SCHEMA_MIDDLE}" \
-        "/data/${PBF_UPTODATE_FILENAME}"
+      -c "CREATE SCHEMA IF NOT EXISTS ${PG_SCHEMA_IMPORT} AUTHORIZATION ${PG_ENV_POSTGRES_USER};"
+  fi
 
-    ${O2P_DIRECTORY}osm2pgsql-replication init \
-      --middle-schema="${PG_SCHEMA_MIDDLE}" \
-      --server https://planet.osm.org/replication/minute/
+  echo "run osm import"
 
-    echo "current replication status: "
-    ${O2P_DIRECTORY}osm2pgsql-replication status --middle-schema="${PG_SCHEMA_MIDDLE}"
+  psql \
+    -c "CREATE SCHEMA IF NOT EXISTS ${PG_SCHEMA_MIDDLE} AUTHORIZATION ${PG_ENV_POSTGRES_USER};"
+  osm2pgsql \
+    --create \
+    --slim \
+    --cache 2000 \
+    --output=flex \
+    --style=/config/parkraum.lua \
+    --middle-schema="${PG_SCHEMA_MIDDLE}" \
+    "/data/${PBF_UPTODATE_FILENAME}"
 
-    echo "run spatial filter after import"
-    /bin/bash /config/run_spatialfilter.sh
+  osm2pgsql-replication init \
+    --middle-schema="${PG_SCHEMA_MIDDLE}" \
+    --server https://planet.osm.org/replication/minute/
+
+#    echo "run spatial filter after import"
+#    /bin/bash /config/run_spatialfilter.sh
 }
 
 function updateosm () {
 
-    echo "run OSM update"
-    ${O2P_DIRECTORY}osm2pgsql-replication update \
-      --middle-schema="${PG_SCHEMA_MIDDLE}" \
-      --max-diff-size "$PG_OSM2PGSQL_DIFFSIZE" \
-      --post-processing /config/run_spatialfilter.sh \
-      -- -O flex -S /config/parkraum.lua \
-      || return $?
-
-    echo "current replication status: "
-    ${O2P_DIRECTORY}osm2pgsql-replication status --middle-schema="${PG_SCHEMA_MIDDLE}"
+  echo "run OSM update"
+  osm2pgsql-replication update \
+    --middle-schema="${PG_SCHEMA_MIDDLE}" \
+    --max-diff-size "$PG_OSM2PGSQL_DIFFSIZE" \
+    -- -O flex -S /config/parkraum.lua \
+    || return $?
 
 }
 
 function import_spatialfilter () {
 
-    PG_SCHEMA_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM information_schema.schemata WHERE schema_name = '${PG_SCHEMA_META}');")
+  PG_SCHEMA_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM information_schema.schemata WHERE schema_name = '${PG_SCHEMA_META}');")
 
-    if [ ! "${PG_SCHEMA_EXISTS}" = "t" ]
+  if [ ! "${PG_SCHEMA_EXISTS}" = "t" ]
+  then
+    echo "create schema ${PG_SCHEMA_META}"
+    psql \
+      -c "CREATE SCHEMA IF NOT EXISTS ${PG_SCHEMA_META} AUTHORIZATION ${PG_ENV_POSTGRES_USER};"
+  fi
+
+  #TODO inform when there is no filter
+  for GEOJSONFILE in "/data"/*.geojson
+  do
+    echo "processing geojson file: ${GEOJSONFILE}"
+    PG_TABLE_SPATIALFILTER_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = '${PG_SCHEMA_META}' AND tablename = 'spatialfilter');")
+    LAYERNAME=$(basename "${GEOJSONFILE}" .geojson)
+
+    if [ ! "${PG_TABLE_SPATIALFILTER_EXISTS}" = "t" ]
     then
-        echo "create schema ${PG_SCHEMA_META}"
-        psql \
-          -c "CREATE SCHEMA IF NOT EXISTS ${PG_SCHEMA_META} AUTHORIZATION ${PG_ENV_POSTGRES_USER};"
+      echo "create spatial filter table"
+      #echo ogr2ogr -lco GEOMETRY_NAME=geom -lco SCHEMA="${PG_SCHEMA_META}" -nlt PROMOTE_TO_MULTI -nln spatialfilter -sql "SELECT '${LAYERNAME}' AS name FROM ${LAYERNAME}" -f "PostgreSQL" PG:service="${PGSERVICE}" "${GEOJSONFILE}"
+      ogr2ogr \
+        -lco GEOMETRY_NAME=geom \
+        -lco SCHEMA="${PG_SCHEMA_META}" \
+        -nlt PROMOTE_TO_MULTI \
+        -nln "${PG_SCHEMA_META}".spatialfilter \
+        -sql "SELECT '${LAYERNAME}' AS name, name as label FROM ${LAYERNAME}" \
+        -f "PostgreSQL" PG:service="${PGSERVICE}" \
+        "${GEOJSONFILE}"
+      rm "${GEOJSONFILE}"
+    else
+      echo "append spatial filter table"
+      ogr2ogr \
+        -nlt PROMOTE_TO_MULTI \
+        -append \
+        -sql "SELECT '${LAYERNAME}' AS name, name as label FROM ${LAYERNAME}" \
+        -nln "${PG_SCHEMA_META}".spatialfilter \
+        -f "PostgreSQL" PG:service="${PGSERVICE}" \
+        "${GEOJSONFILE}"
+      rm "${GEOJSONFILE}"
     fi
+  done
+  echo "import spatial filter done"
 
-    #TODO inform when there is no filter
-    for GEOJSONFILE in "/data"/*.geojson
-    do
-        echo "processing geojson file: ${GEOJSONFILE}"
-        PG_TABLE_SPATIALFILTER_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = '${PG_SCHEMA_META}' AND tablename = 'spatialfilter');")
-        LAYERNAME=$(basename "${GEOJSONFILE}" .geojson)
-
-        if [ ! "${PG_TABLE_SPATIALFILTER_EXISTS}" = "t" ]
-        then
-            echo "create spatial filter table"
-            #echo ogr2ogr -lco GEOMETRY_NAME=geom -lco SCHEMA="${PG_SCHEMA_META}" -nlt PROMOTE_TO_MULTI -nln spatialfilter -sql "SELECT '${LAYERNAME}' AS name FROM ${LAYERNAME}" -f "PostgreSQL" PG:service="${PGSERVICE}" "${GEOJSONFILE}"
-            ogr2ogr \
-              -lco GEOMETRY_NAME=geom \
-              -lco SCHEMA="${PG_SCHEMA_META}" \
-              -nlt PROMOTE_TO_MULTI \
-              -nln spatialfilter \
-              -sql "SELECT '${LAYERNAME}' AS name FROM ${LAYERNAME}" \
-              -f "PostgreSQL" PG:service="${PGSERVICE}" \
-              "${GEOJSONFILE}"
-            rm "${GEOJSONFILE}"
-        else
-            echo "append spatial filter table"
-            ogr2ogr \
-              -nlt PROMOTE_TO_MULTI \
-              -append \
-              -sql "SELECT '${LAYERNAME}' AS name FROM ${LAYERNAME}" \
-              -nln "${PG_SCHEMA_META}".spatialfilter \
-              -f "PostgreSQL" PG:service="${PGSERVICE}" \
-              "${GEOJSONFILE}"
-            rm "${GEOJSONFILE}"
-        fi
-    done
-    echo "import spatial filter done"
+  psql -f /config/spatialfilter_cleanup.sql
+  echo "cleanup table spatialfilter done"
 }
 
 while : ; do
 
-    if [ ! $(find /data/ -name "*.geojson" | wc -l) -eq 0 ] ; then
-      echo "importing spatialfilter..."
-      import_spatialfilter
-    fi
+  if [ ! $(find /data/ -name "*.geojson" | wc -l) -eq 0 ] ; then
+    echo "importing spatialfilter..."
+    import_spatialfilter
+  fi
 
-    if [ ! $(psql -t -c "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = '${PG_SCHEMA_IMPORT}' AND tablename = 'highways');") = "t" ] ; then
-      echo "starting import..."
-      importosm
-    fi
+  PG_SCHEMA_MIDDLE_EXISTS=$(psql -XAt -c "SELECT EXISTS (SELECT FROM information_schema.schemata WHERE schema_name = '${PG_SCHEMA_MIDDLE}');")
+  if [ "${PG_SCHEMA_MIDDLE_EXISTS}" = "t" ] ; then
+    echo "current replication status: "
+    osm2pgsql-replication status --middle-schema="${PG_SCHEMA_MIDDLE}"
+  fi
 
-    updateosm
-    [ "$LOOP" -eq 0 ] && exit $?
-    sleep "$LOOP" || exit
+  if [ ! $(psql -t -c "SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = '${PG_SCHEMA_IMPORT}' AND tablename = 'highways');") = "t" ] ; then
+    echo "starting import..."
+    importosm
+  fi
+
+  updateosm
+  [ "$LOOP" -eq 0 ] && exit $?
+  sleep "$LOOP" || exit
 done
